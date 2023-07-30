1782b37cbd4c9fd9c42439e9a9f030330f7e1e9b
Roman Didusenko <albertushoffman@proton.me>
2023-07-30 19:56:22 +0300

    added cors policy

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 
-let products = require('./products.json')
+let cors = require('cors');
+
+let products = require('./products.json');
 
 const PORT = 5000;
 
 const app = express();
+
+app.use(cors());
 
 app.get('/products', (request, response) => {
   response.send(products);
