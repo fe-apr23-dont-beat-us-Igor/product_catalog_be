22130453854464faef302bc2937c4152f9108110
Roman Didusenko <albertushoffman@proton.me>
2023-07-31 15:26:05 +0300

    loading products from db

--- src/server.ts
+++ src/server.ts
@@ -2,33 +2,13 @@ import express from 'express';
 import { initDB } from './initDB';
 
 import sequelize from 'sequelize-typescript';
+import { Product } from './models/Product.model';
 
 let cors = require('cors');
 
 // postgres://products_db_74rl_user:O4Bzs9v7kCIbO7uiiSJhcXIpLhC8ivWs@dpg-cj3lk8tiuie55plnr410-a.frankfurt-postgres.render.com/products_db_74rl
 
 const serverInit = async () => {
-  let products = require('./products.json');
-
-  products = products.map((product: { name: any; capacity: any; priceRegular: any; priceDiscount: any; images: any; screen: any; ram: any; }) => {
-    const {
-      name,
-      capacity,
-      priceRegular,
-      priceDiscount,
-      screen,
-      ram,
-    } = product;
-    
-    return {
-      name,
-      capacity,
-      priceRegular,
-      priceDiscount,
-      screen,
-      ram,
-    }
-  });
 
   const PORT = 5000;
 
@@ -39,6 +19,8 @@ const serverInit = async () => {
   const sequelize = initDB();
 
   const res = await sequelize.authenticate();
+
+  let products = Product.findAll();
 
   app.get('/products', (request, response) => {
     response.send(products);
