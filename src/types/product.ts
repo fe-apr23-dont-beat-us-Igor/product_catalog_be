3a740e4ee2a71aacee837c47d4c713630c232036
Roman Didusenko <albertushoffman@proton.me>
2023-08-02 08:56:20 +0300

    added type

---
+++ src/types/product.ts
@@ -0,0 +1,14 @@
+export interface Product {
+  id: number,
+  itemId: string,
+  category: string,
+  name: string,
+  capacity: string,
+  fullPrice: number,
+  color: string,
+  price: number,
+  screen: string,
+  ram: string,
+  year: string,
+  image_id: number,
+}
