c656e252c5227904fb5349c11549e854c180f1d5
Roman Didusenko <albertushoffman@proton.me>
2023-08-10 09:24:27 +0300

    add auth

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,8 +1,18 @@
 import { Response, Request } from 'express';
 const bcrypt = require('bcrypt');
 const { validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const { secret } = require('./auth.config.js');
 
 import { User } from "../models/User.model";
+
+const generateAccessToken = (username: any) => {
+  const payload = {
+    username,
+  }
+
+  return jwt.sign(payload, secret, {expiresIn: "24h"})
+}
 
 class authController {
   async registration(req: Request, res: Response) {
@@ -14,6 +24,7 @@ class authController {
           message: 'Registration error',
           errors,
         })
+        return;
       }
       const {username, password} = req.body;
       const candidate = await User.findOne({
@@ -25,6 +36,7 @@ class authController {
       if (candidate) {
         res.statusCode = 400;
         res.send('User with this username already exists');
+        return;
       }
 
       const hashPassword = bcrypt.hashSync(password, 7);
@@ -41,11 +53,33 @@ class authController {
 
   async login(req: Request, res: Response) {
     try {
-      
+      const { username, password } = req.body;
+      const user = await User.findOne({
+        where: {
+          username: username,
+        }
+      });
+
+      if (!user) {
+        res.statusCode = 400;
+        res.send(`User ${username} not found!`);
+        return;
+      }
+
+      const validPassword = bcrypt.compareSync(password, user.password);
+
+      if (!validPassword) {
+        res.statusCode = 400;
+        res.send(`Incorrect password!`);
+        return;
+      }
+    
+      const token = jwt.sign({id: user.id}, secret, {expiresIn: '24h'});
+      res.send(token);
     } catch (error) {
       console.log(error);
       res.statusCode = 400;
-      res.send('Login error!');
+      res.send('Login error');
       return;
     }
   }
